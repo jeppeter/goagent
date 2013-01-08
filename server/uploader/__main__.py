e626f115bc69f36edd44ab5332d27aef81118578
jeppeter <jeppeter@gmail.com>
2013-01-08 20:50:41 +0800

    init goagent in 2.0.12 revision

---
+++ server/uploader/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+def main():
+    import appcfg
+    appcfg.main()
+
+if __name__ == '__main__':
+   try:
+       main()
+   except KeyboardInterrupt:
+       pass
